c81b5dae2490346c673982f4c8f6e8891bb40b50
Harikrishna C <harikrishnac005@gmail.com>
2025-08-11 12:15:32 +0530

    chore: Update documentation & TODO update

--- core_main/faiss-file-indexing.py
+++ core_main/faiss-file-indexing.py
@@ -1,11 +1,16 @@
+# This file is for going through all the files in a directory, 
+# open and read it contents and converting that data to vectors and then using faiss to store the vectors and for semantic search
 import os
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # embeddings = OllamaEmbeddings(
 #     model="qllama/bge-small-en-v1.5",
 # )
 
-
-#Implemented walkthrough of all files with folder exclusions
+# TODO - Pass extension, file/folder exclusions as arguments
+# TODO - Add text splitting functionality
+ 
+# Implemented walkthrough of all files with folder exclusions
 def walkthrough_files(extensions=None):
     current_working_dir = os.getcwd()
     file_paths = []
@@ -16,13 +21,17 @@ def walkthrough_files(extensions=None):
             if extensions is None or file.endswith(tuple(extensions)):
                 file_paths.append(os.path.join(root, file))
     return file_paths
-#
+
+# Implemented opening and reading of files
 def open_files(file_dir):
     file_contents = []
     for dirs in file_dir:
          with open(dirs, "r", errors="ignore") as file:
              file_contents.append(file.read())
     return file_contents
+
+# def split_text_and_embed():
+
 
 dirs = walkthrough_files(extensions=[".py"])
 print(open_files(dirs))
